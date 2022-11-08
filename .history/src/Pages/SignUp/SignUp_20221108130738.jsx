b860a6b4c57fd5a0d92809b0eb8b70f8f528636a
unknown <izazahmed0177@gmail.com>
2022-11-08 13:25:15 +0600

    google sin in  complite

--- .history/src/Pages/SignUp/SignUp_20221108130738.jsx
+++ .history/src/Pages/SignUp/SignUp_20221108130738.jsx
@@ -21,7 +21,6 @@ const SignUp = () => {
             // setAuthToken(user)
 
             handleUpdateUserProfil(name,photoURL)
-            form.reset();
         })
         .catch(err=>console.error(err))
     }
@@ -56,11 +55,11 @@ const SignUp = () => {
     <form onSubmit={handaleSignUp} className="mt-6">
         <div>
             <label  className="block text-sm text-gray-800 dark:text-gray-200">Username</label>
-            <input name='name'placeholder='User Name' type="text" required className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+            <input name='name'placeholder='User Name' type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
         </div>
         <div>
             <label  className="block text-sm text-gray-800 dark:text-gray-200">Photo URL</label>
-            <input name='photoURL' placeholder='Photo URL'required type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+            <input name='photoURL' placeholder='Photo URL' type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
         </div>
         <div>
             <label className="block text-sm text-gray-800 dark:text-gray-200">User Email</label>
